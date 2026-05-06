c9cd839a714c7ec6ff5a55c00567a44999c89222
nhl007 <apar.asif.an@gmail.com>
2026-05-07 01:44:02 +0600

    fix: Restore props spread in LockIcon component for proper functionality

--- assets/icons/lock.tsx
+++ assets/icons/lock.tsx
@@ -1,7 +1,6 @@
 import Svg, { Path, Rect, SvgProps } from "react-native-svg";
 const LockIcon = (props: SvgProps) => (
   <Svg
-    {...props}
     width={24}
     height={24}
     fill="none"
@@ -10,6 +9,7 @@ const LockIcon = (props: SvgProps) => (
     strokeLinejoin="round"
     strokeWidth={2}
     viewBox="0 0 24 24"
+    {...props}
   >
     <Rect width={18} height={11} x={3} y={11} rx={2} ry={2} />
     <Path d="M7 11V7a5 5 0 0 1 10 0v4" />
